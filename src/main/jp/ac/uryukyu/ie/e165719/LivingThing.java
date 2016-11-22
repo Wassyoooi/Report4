129ec1def782d71664f116e1b227745026f37455
Takahiro Iha <e165719@ie.u-ryukyu.ac.jp>
2016-11-22 12:09:12 +0900

    ステップ3

--- src/main/jp/ac/uryukyu/ie/e165719/LivingThing.java
+++ src/main/jp/ac/uryukyu/ie/e165719/LivingThing.java
@@ -1,10 +1,10 @@
 package jp.ac.uryukyu.ie.e165719;
 
 public class LivingThing {
-    String name;
-    int hitPoint;
-    int attack;
-    boolean dead;
+    private String name;
+    private int hitPoint;
+    private int attack;
+    private boolean dead;
 
     public LivingThing (String name, int hitPoint, int attack) {
         this.name = name;
@@ -13,9 +13,20 @@ public class LivingThing {
         dead = false;
         System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, hitPoint, attack);
     }
+    public int getHitPoint() {
+        return hitPoint;
+    }
+
+    public void setHitPoint(int hitPoint) {
+        this.hitPoint = hitPoint;
+    }
 
     public boolean isDead() {
         return dead;
+    }
+
+    public void setDead(boolean dead) {
+        this.dead = dead;
     }
 
     public String getName() {
